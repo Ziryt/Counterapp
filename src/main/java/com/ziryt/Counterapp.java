59cd87f5295c31f8bc8417d5e5deaf1211e576d4
Ziryt <alexander.kolomiets@superdispatch.com>
2024-03-17 20:05:54 +0100

    init

---
+++ src/main/java/com/ziryt/Counterapp.java
@@ -0,0 +1,11 @@
+package com.ziryt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Counterapp {
+    public static void main(String[] args) {
+        SpringApplication.run(Counterapp.class, args);
+    }
+}
