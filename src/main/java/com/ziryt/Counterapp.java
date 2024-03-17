c2b09aa8632e53b369c6a48155494030e8398552
Ziryt <alexander.kolomiets@superdispatch.com>
2024-03-17 20:27:49 +0100

    first hello endpoint application.yml added

--- src/main/java/com/ziryt/Counterapp.java
+++ src/main/java/com/ziryt/Counterapp.java
@@ -2,10 +2,18 @@ package com.ziryt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Counterapp {
     public static void main(String[] args) {
         SpringApplication.run(Counterapp.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
     }
 }
