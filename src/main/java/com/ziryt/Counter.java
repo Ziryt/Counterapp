b348661565d218484ff9d8e7381b870955107363
Ziryt <alexander.kolomiets@superdispatch.com>
2024-03-20 21:23:53 +0100

    implemented basic CRUD

--- src/main/java/com/ziryt/Counter.java
+++ src/main/java/com/ziryt/Counter.java
@@ -1,28 +1,48 @@
 package com.ziryt;
 
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import java.util.Objects;
 
+@Entity
 public class Counter {
+
+    @Id
+    @SequenceGenerator(
+            name = "counter_id_generator",
+            sequenceName = "counter_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "counter_id_generator"
+    )
     private Integer Id;
     private String name;
     private Integer count;
-    private Integer limit;
+    private Integer limited;
     private String color;
 
     public Counter(Integer id,
                    String name,
                    Integer count,
-                   Integer limit,
+                   Integer limited,
                    String color) {
         Id = id;
         this.name = name;
         this.count = count;
-        this.limit = limit;
+        this.limited = limited;
         this.color = color;
     }
 
     public Counter() {
     }
+
+
 
     public Integer getId() {
         return Id;
@@ -48,12 +68,12 @@ public class Counter {
         this.count = count;
     }
 
-    public Integer getLimit() {
-        return limit;
+    public Integer getLimited() {
+        return limited;
     }
 
-    public void setLimit(Integer limit) {
-        this.limit = limit;
+    public void setLimited(Integer limited) {
+        this.limited = limited;
     }
 
     public String getColor() {
@@ -72,13 +92,13 @@ public class Counter {
         return Objects.equals(Id, counter.Id)
                 && Objects.equals(name, counter.name)
                 && Objects.equals(count, counter.count)
-                && Objects.equals(limit, counter.limit)
+                && Objects.equals(limited, counter.limited)
                 && Objects.equals(color, counter.color);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Id, name, count, limit, color);
+        return Objects.hash(Id, name, count, limited, color);
     }
 
     @Override
@@ -87,7 +107,7 @@ public class Counter {
                 "Id=" + Id +
                 ", name='" + name + '\'' +
                 ", count=" + count +
-                ", limit=" + limit +
+                ", limit=" + limited +
                 ", color='" + color + '\'' +
                 '}';
     }
